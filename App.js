be1862d3843fdd70e4325a2581f1df5d9c1d4ed5
Alexey Prosvirnin <aprosv@ya.ru>
2022-05-22 02:16:48 +0300

    styling lesson

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+     <View style={styles.toolBar}> 
+      <TextInput style={styles.textInput} placeholder='Your course Goals'/>
+      <Button title='Add Goal'/>
+     </View>
+     <View>
+       <Text>List of goals...</Text>
+     </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    padding:50
   },
+  toolBar:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#ccc',
+    width:'80%',
+    marginRight:8
+  }
 });
